263173efafe6683bd8d768650c9ceddeca543892
DMulhausen <DMulhausen@KPLAXLT0286.alpha.kaplaninc.com>
2018-04-11 21:21:21 -0500

    constructor created

--- libraryAPI/src/libraryAPI/libraryAPI.java
+++ libraryAPI/src/libraryAPI/libraryAPI.java
@@ -1,14 +1,26 @@
 package libraryAPI;
 
 public class libraryAPI {
+	String apiHead;
+	String apiTail;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-	public libraryAPI(String apiHead,String terms, String apiTail)
-	{
+	public libraryAPI(String start, String tail)
+	{//constructor to set up API for multiple queries
+		apiHead=start;
+		apiTail=tail;
+	}
+	public String query(String terms)
+	{//returns the result of a query
+		String result="";
 		
+		
+		
+		
+		return result;
 	}
 
 }
